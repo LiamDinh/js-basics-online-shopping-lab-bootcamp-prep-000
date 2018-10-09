831ba2fed198d5997cac308202edbb587bc60c23
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-09 18:20:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function total() {
   // write your code here
    var total = 0;
   for(var i = 0; i < cart.length; i++) {
-    total = total + cart[i].price 
+    total = total + cart[i].itemPrice 
   }
   return total
 }
